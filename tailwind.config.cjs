d4cd073a3fcac1dbd9eb769f7120cda9bd13cc6f
Manuel <manu@meetsam.io>
2023-10-05 11:02:53 +0200

    more refactors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,4 +5,16 @@ module.exports = {
 		extend: {},
 	},
 	plugins: [],
+	theme: {
+	  fontSize: {
+		sm: '0.6rem',
+		base: '0.8rem',
+		xl: '1rem',
+		'2xl': '1.25rem',
+		'3xl': '1.563rem',
+		'4xl': '1.953rem',
+		'5xl': '2.441rem',
+		'6xl': '3.052rem',
+	  }
+	}
 }
